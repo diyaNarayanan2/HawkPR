2c03edafec7c8060b7a88feed830eafcda6863bf
diyaNarayanan2 <diya.narayanan22@gmail.com>
2024-09-20 21:28:04 +0530

    output paths update

--- main.m
+++ main.m
@@ -15,8 +15,8 @@ Delta = 3;
 SimTimes = 6;
 
 %to_csv function will automatically create a csv file with this path
-OutputPath_pred = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\output";
-OutputPath_mdl = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\output";
+OutputPath_pred = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\output\model_pred.csv";
+OutputPath_mdl = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\output\mdl.mat";
 InputPath_demography = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\input_data\Demo_Dconfirmed.csv";
 InputPath_report = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\input_data\NYT_Dconfirmed.csv";
 InputPath_mobility = "C:\Users\dipiy\OneDrive\Documents\GitHub\HawkPR\input_data\GoogleMobi_Dconfirmed.csv";
