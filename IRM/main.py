c18971d7002d5e7be316e7bca0864937c282d72e
diyaNarayanan2 <diya.narayanan22@gmail.com>
2024-10-01 17:48:33 +0530

    #labels in main.py

--- IRM/main.py
+++ IRM/main.py
@@ -13,9 +13,11 @@ import torch
 import numpy
 
 
+# returns a pretty 1d line of all values in vector, eg "[+0.112 -2.333]"
 def pretty(vector):
     vlist = vector.view(-1).tolist()
     return "[" + ", ".join("{:+.3f}".format(vi) for vi in vlist) + "]"
+    # .3f upto 3 decimal places float
 
 
 def errors(w, w_hat):
@@ -51,7 +53,8 @@ def run_experiment(args):
             args["setup_ones"],
             args["setup_hidden"],
             args["setup_hetero"],
-            args["setup_scramble"])
+            args["setup_scramble"],
+        )
     elif args["setup_sem"] == "icp":
         setup_str = "sem_icp"
     else:
@@ -60,13 +63,13 @@ def run_experiment(args):
     all_methods = {
         "ERM": EmpiricalRiskMinimizer,
         "ICP": InvariantCausalPrediction,
-        "IRM": InvariantRiskMinimization
+        "IRM": InvariantRiskMinimization,
     }
 
     if args["methods"] == "all":
         methods = all_methods
     else:
-        methods = {m: all_methods[m] for m in args["methods"].split(',')}
+        methods = {m: all_methods[m] for m in args["methods"].split(",")}
 
     all_sems = []
     all_solutions = []
@@ -74,13 +77,17 @@ def run_experiment(args):
 
     for rep_i in range(args["n_reps"]):
         if args["setup_sem"] == "chain":
-            sem = ChainEquationModel(args["dim"],
-                                     ones=args["setup_ones"],
-                                     hidden=args["setup_hidden"],
-                                     scramble=args["setup_scramble"],
-                                     hetero=args["setup_hetero"])
+            sem = ChainEquationModel(
+                args["dim"],
+                ones=args["setup_ones"],
+                hidden=args["setup_hidden"],
+                scramble=args["setup_scramble"],
+                hetero=args["setup_hetero"],
+            )
 
+            # converts args env_list into a list of float env values
             env_list = [float(e) for e in args["env_list"].split(",")]
+            # stores the diff chain models made for each environment
             environments = [sem(args["n_samples"], e) for e in env_list]
         else:
             raise NotImplementedError
@@ -88,52 +95,71 @@ def run_experiment(args):
         all_sems.append(sem)
         all_environments.append(environments)
 
+    # zip function jointly iterates values from sems and envs
     for sem, environments in zip(all_sems, all_environments):
         sem_solution, sem_scramble = sem.solution()
 
         solutions = [
-            "{} SEM {} {:.5f} {:.5f}".format(setup_str,
-                                             pretty(sem_solution), 0, 0)
+            "{} SEM {} {:.5f} {:.5f}".format(setup_str, pretty(sem_solution), 0, 0)
         ]
+        # creates a string statement which summarizes soln
 
         for method_name, method_constructor in methods.items():
             method = method_constructor(environments, args)
 
             method_solution = sem_scramble @ method.solution()
 
+            # diff b/w true soln and method soln
             err_causal, err_noncausal = errors(sem_solution, method_solution)
 
-            solutions.append("{} {} {} {:.5f} {:.5f}".format(
-                setup_str,
-                method_name,
-                pretty(method_solution),
-                err_causal,
-                err_noncausal))
+            solutions.append(
+                "{} {} {} {:.5f} {:.5f}".format(
+                    setup_str,
+                    method_name,
+                    pretty(method_solution),
+                    err_causal,
+                    err_noncausal,
+                )
+            )
 
         all_solutions += solutions
 
     return all_solutions
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Invariant regression')
-    parser.add_argument('--dim', type=int, default=10)
-    parser.add_argument('--n_samples', type=int, default=1000)
-    parser.add_argument('--n_reps', type=int, default=10)
-    parser.add_argument('--skip_reps', type=int, default=0)
-    parser.add_argument('--seed', type=int, default=0)  # Negative is random
-    parser.add_argument('--print_vectors', type=int, default=1)
-    parser.add_argument('--n_iterations', type=int, default=100000)
-    parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--verbose', type=int, default=0)
-    parser.add_argument('--methods', type=str, default="ERM,ICP,IRM")
-    parser.add_argument('--alpha', type=float, default=0.05)
-    parser.add_argument('--env_list', type=str, default=".2,2.,5.")
-    parser.add_argument('--setup_sem', type=str, default="chain")
-    parser.add_argument('--setup_ones', type=int, default=1)
-    parser.add_argument('--setup_hidden', type=int, default=0)
-    parser.add_argument('--setup_hetero', type=int, default=0)
-    parser.add_argument('--setup_scramble', type=int, default=0)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Invariant regression")
+    # Dimension of x,y,z - square matrices of data
+    parser.add_argument("--dim", type=int, default=10)
+    # No. of Samples: n in sem call
+    parser.add_argument("--n_samples", type=int, default=1000)
+
+    parser.add_argument("--n_reps", type=int, default=10)
+    parser.add_argument("--skip_reps", type=int, default=0)
+    parser.add_argument("--seed", type=int, default=0)  # Negative is random
+    parser.add_argument("--print_vectors", type=int, default=1)
+
+    # Training iterations
+    parser.add_argument("--n_iterations", type=int, default=100000)
+
+    # Train process learning rate
+    parser.add_argument("--lr", type=float, default=1e-3)
+
+    # Verbose
+    parser.add_argument("--verbose", type=int, default=0)
+
+    # Methods to test
+    parser.add_argument("--methods", type=str, default="ERM,ICP,IRM")
+    parser.add_argument("--alpha", type=float, default=0.05)
+    parser.add_argument("--env_list", type=str, default=".2,2.,5.")
+    # default only 3 envs
+
+    # CHAIN EQ MODEL- ARGS for Setup
+    parser.add_argument("--setup_sem", type=str, default="chain")
+    parser.add_argument("--setup_ones", type=int, default=1)
+    parser.add_argument("--setup_hidden", type=int, default=0)
+    parser.add_argument("--setup_hetero", type=int, default=0)
+    parser.add_argument("--setup_scramble", type=int, default=0)
     args = dict(vars(parser.parse_args()))
 
     all_solutions = run_experiment(args)
